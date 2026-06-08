9a8c1274f4f493468c0c05988976153e2d914d14
RohanDaCoder <rohanbmgo2010@gmail.com>
2026-06-08 11:22:13 +0600

    Setup Clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+    #[arg(short, long)]
+    folder: PathBuf,
+
+    #[arg(short, long, default_value_t = false)]
+    dry_run: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("🔍 Scanning folder: {}", args.folder.display());
 }
